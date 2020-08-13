afca0cbd9b5b6cdd1121508c9be1a9bb550aaf0c
chi zhang <chizhang0401@gmail.com>
2020-08-13 00:16:29 -0700

    day07

---
+++ R/day-06.R
@@ -0,0 +1,54 @@
+# Chi Zhang
+### 08/11/2020
+# Daily-exercise-06
+
+library(tidyverse)
+
+url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
+
+covid = read_csv(url)
+
+
+### Question 1
+
+covid %>%
+  filter(date == max(date)) %>%
+  group_by(state) %>%
+  summarize(cases = sum(cases, na.rm = TRUE)) %>%
+  ungroup() %>%
+  slice_max(cases, n = 6) %>%
+  pull(state) ->
+  top_states
+
+filter(covid, state %in% c("California", "Florida", "Texas", "New York", "Georgia", "Illinois")) %>%
+  group_by(state, date) %>%
+  summarise(cases = sum(cases)) %>%
+  ungroup() %>%
+  ggplot(aes(x = date, y = cases)) +
+  geom_line(aes(color = state)) +
+  labs(title = "6 States with Most Cases: COVID-19 Pandemic",
+       x = "Date",
+       y = "Everyday Cases",
+       caption = "based on https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv",
+       subtitle = 'Data Source: Covid',
+       color = "") +
+  facet_wrap(~state) +
+  theme_gray()
+
+
+### Question 2
+
+covid %>%
+  group_by(date) %>%
+  summarise(cases = sum(cases, na.rm = TRUE)) %>%
+  ungroup() %>%
+  ggplot(aes(x = date, y = cases)) +
+  geom_col(color = "red") +
+  labs(title = "National Cummulative Case Counts: COVID-19 Pandemic",
+       x = "Date",
+       y = "Everyday Cases",
+       caption = "based on https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv",
+       subtitle = 'Data Source: Covid',
+       color = "") +
+  theme_gray()
+
---
+++ R/day-07.R
@@ -0,0 +1,35 @@
+# Chi Zhang
+# 08/12/2020
+# Daily-exercise-07
+
+library(tidyverse)
+
+homes = read.csv('data/landdata-states.csv')
+
+covid = read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
+
+pop = readxl:::read_excel("data/populationEstimates.xls", skip = 2) %>%
+  select(pop2019 = POP_ESTIMATE_2019, State = State, fips = FIPStxt)
+
+areadata = inner_join(pop, homes, by = "State") %>%
+  select(region, fips)
+
+Regioncases = inner_join(covid, areadata, by = "fips")%>%
+  select(date, region,cases, deaths)
+
+REGION = Regioncases %>%
+  group_by(date, region) %>%
+  summarise(cases = max(cases), deaths = max(deaths)) %>%
+  pivot_longer(cols = c('cases', 'deaths')) %>%
+  ungroup()
+
+REGION %>%
+  ggplot(aes(x = date, y = value)) +
+  geom_line(aes(color = region)) +
+  labs(title = "Cumulative Cases and Deaths: Region",
+       x = "Date",
+       y = "Daily Cumulative count",
+       caption = "based on https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv",
+       subtitle = 'Data Source: NY-Times',
+       color = "") +
+  facet_grid(name~region, scales = "free_y")
