d0cd7ddd70ca23dfef53b1410755b5d806130e5d
chi zhang <chizhang0401@gmail.com>
2020-08-20 19:37:14 -0700

    day12

---
+++ R/day12.R
@@ -0,0 +1,34 @@
+# Chi Zhang
+### 8/20/2020
+# Daily-exercise-12
+
+
+library(tidyverse)
+library(sf)
+library(USAboundaries)
+
+state = USAboundaries::us_states() %>%
+  filter(!name %in% c("Alaska", "Hawaii", "Puerto Rico"))
+#  st_as_sf(coords = c("lng", "lat"), crs = 4326)
+
+Idaho = USAboundaries::us_states() %>%
+  filter(state_name == "Idaho")
+#  st_as_sf(coords = c("lng", "lat"), crs = 4326)
+
+Colorado = USAboundaries::us_states() %>%
+  filter(state_name == "Colorado")
+
+IdahoBoundary = st_filter(state, Idaho, .predicate = st_touches)
+
+ColoradoBoundary = st_filter(state, Colorado, .predicate = st_touches)
+
+ggplot() +
+  geom_sf(data = state, fill = NA, size = 1) +
+  geom_sf(data = ColoradoBoundary, fill = 'red', alpha = .5) +
+  labs(title = "States Around Colorado",
+       x = "Longitude",
+       y = "Latitude") +
+  theme_gray()
+
+
+ggsave(file = "img/States Around Colorado.png")
